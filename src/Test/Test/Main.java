b01daa431646f362e3dbe534077a8f6747ac30c4
longdz9x1 <longkoy006@gmail.com>
2019-04-03 10:08:57 +0700

    Add files via upload

---
+++ src/Test/Test/Main.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Main {
+    public static void main(String [] args) {
+        Hotel hotel1 = new Hotel();
+        hotel1.inputInfo();
+        hotel1.printInfo();
+        Hotel hotel2 = new Hotel();
+    }
+}
